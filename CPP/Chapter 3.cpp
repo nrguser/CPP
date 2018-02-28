dee28ba40c1c505bf9d90ac9f0c1d31d62aa4916
Matthew Buck <nrguser@gmail.com>
2018-02-27 21:30:59 -0600

    Initial Dump

---
+++ CPP/Chapter 3.cpp
@@ -0,0 +1,12 @@
+#include "header.h"
+
+int main()
+{
+	cout << "Please enter your First Name (followed by enter):" << endl;
+	string first_name;
+	cin >> first_name;
+	cout << "Hello, " << first_name << endl;
+
+
+	return 0;
+}
---
+++ CPP/Chapter2.cpp
@@ -0,0 +1,22 @@
+#include "header.h"
+
+int main1()
+{
+	cout << "Hello World" << endl;
+
+
+	return 0;
+}
+
+// Revew Questions
+// Question 1
+
+int main2()
+{
+	cout << "Hello, programming!" << endl;
+	cout << "Here we go!" << endl;
+
+	return 0;
+	
+}
+
