efc6304d089f8f623673199df007aaeef9985857
Matthew Buck <nrguser@gmail.com>
2018-02-28 21:54:19 -0600

    Chapter 3 done

--- CPP/Chapter 3.cpp
+++ CPP/Chapter 3.cpp
@@ -1,6 +1,6 @@
 #include "header.h"
 
-int main()
+int main3()
 {
 	cout << "Please enter your First Name (followed by enter):" << endl;
 	string first_name;
@@ -10,3 +10,33 @@ int main()
 
 	return 0;
 }
+
+int main31()
+{
+	string previous = " ";
+	string current;
+	while (cin >> current){
+		if (previous == current)
+			cout << "repeated word " << current << endl;
+	previous = current;
+		}
+
+	return 0;
+}
+
+int main32()
+{
+	int number_of_words = 0;
+	string previous1 = " ";
+	string current1;
+	while (cin >> current1) {
+		++number_of_words;
+		if (previous1 == current1)
+			cout << "Word number " << number_of_words << " repeated " << current1 << endl;
+		previous1 = current1;
+	}
+
+	return 0;
+}
+
+
